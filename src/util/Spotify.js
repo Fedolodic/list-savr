a131b850850719edb08d10dbcf71075597fada5a
David Martinez <david.mtz00@hotmail.com>
2020-06-03 17:03:54 -0500

    Obtain a Spotify Access Token

---
+++ src/util/Spotify.js
@@ -0,0 +1,31 @@
+const clientId = '51f40e475d1a44a2b653b3cc526750e2';
+const redirectUri = 'http://localhost:3000/';
+let accessToken;
+
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken;
+        }
+
+        // Check for an access token match
+        // window.location.href gets the URL
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+
+            // This clears the parameters, allowing us to grab a new access token when it expires
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
+        }
+    }
+};
+
+export default Spotify;
