c166e539da8cdf4206491cf0d34f5a39d0e8c3ee
David Martinez <david.mtz00@hotmail.com>
2020-06-04 20:17:34 -0500

    Fix Add Items to a Playlist Spotify Endpoint

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -50,8 +50,8 @@ export default class App extends React.Component {
     }
 
     savePlaylist() {
-        const trackURIs = this.state.playlistTracks.map(track => track.uri);
-        Spotify.savePlaylist(this.state.playlistName, trackURIs).then(() => {
+        const trackUris = this.state.playlistTracks.map(track => track.uri);
+        Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
             this.setState({
                 playlistName: 'Type Your New Playlist Name',
                 playlistTracks: []
